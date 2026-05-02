16a1470e8ca6d82e3eded3fac1f4e057de873c07
kanijr <zitfitgpo@gmail.com>
2026-05-02 15:31:05 +0300

    Add initial mock train data

---
+++ src/data/trains.js
@@ -0,0 +1,23 @@
+export const trains = [
+  {
+    id: 1,
+    number: "ІС+ 705",
+    from: "Київ",
+    to: "Львів",
+    departureTime: "07:20",
+  },
+  {
+    id: 2,
+    number: "043К",
+    from: "Київ",
+    to: "Івано-Франківськ",
+    departureTime: "18:30",
+  },
+  {
+    id: 3,
+    number: "081Л",
+    from: "Львів",
+    to: "Київ",
+    departureTime: "21:05",
+  },
+];
