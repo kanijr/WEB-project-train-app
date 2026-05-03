675ea443bf9fd6be93e959ac7ad64ca7480e0cb3
kanijr <zitfitgpo@gmail.com>
2026-05-03 11:28:01 +0300

    Add react-router-dom and basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<Navigate to={"/"} replace />} />
+    </Routes>
+  );
 }
 
 export default App;
